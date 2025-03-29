a81723135bc45128aacdbb151eb09de9abf94718
himanid27 <138992332+HimaniD27@users.noreply.github.com>
2025-03-29 15:54:10 +0530

    creating table

--- stock_market_analysis.sql
+++ stock_market_analysis.sql
@@ -3,4 +3,14 @@ create database stocks_analysis;
 use stocks_analysis;
 
 create table market_data(
-	);
+	stock_id varchar(10),
+    trade_date date,
+    open_price decimal(9,2),
+    close_price decimal(9,2),
+    high_price decimal(9,2),
+    low_price decimal(9,2),
+    volume int,
+    primary key (stock_id, trade_date));
+    
+select * 
+from market_data;
